d1f2a6d6f9424f9ea2efff41a237fab43f674ba1
Toby Flemming <toby.flemming@williamhill.com.au>
2016-08-11 16:51:11 +1000

    Optimize 'Store Credit'

--- 2010/qualification/store-credit/index.js
+++ 2010/qualification/store-credit/index.js
@@ -1,20 +1,37 @@
 const forEachCase = require('./cases.js');
 
-forEachCase('./small.in', (args) => {
-  const credit = parseInt(args[0]);
-  let items = args[2].split(' ').map((item) => parseInt(item));
+const indexesAscending = (a, b) => {
+  a++;
+  b++;
 
-  /*
-  // items.sort((a, b) => a - b);
-  Need to keep track of original indexes
+  if (a < b) {
+    return `${a} ${b}`;
+  } else {
+    return `${b} ${a}`;
+  }
+}
+
+forEachCase('./large.in', (args) => {
+  const credit = parseInt(args[0]);
+
+  // Convert each element in array to an object to keep track of 'originalIndex'
+  let items = args[2].split(' ').map((item, i) => {
+    return {
+      originalIndex: i,
+      value: parseInt(item)
+    }
+  });
+
+  // Sort in ascending order
+  items.sort((a, b) => a.value - b.value);
 
   //Find the highest sum that is less than or equal to 'credit'
-  const lowest = items[0];
+  const lowest = items[0].value;
   for (var i = items.length - 1; i > 0; i--) {
-    let sum = lowest + items[i];
+    let sum = lowest + items[i].value;
 
     if (sum === credit) {
-      return `1 ${i + 1}`;
+      return indexesAscending(items[0].originalIndex, items[i].originalIndex);
     } else if (sum < credit) {
       break;
     }
@@ -22,13 +39,12 @@ forEachCase('./small.in', (args) => {
 
   // Discard all items greater than 'i'
   items = items.slice(0, i + 1);
-  */
 
   for (let i = 0; i < items.length; i++) {
     for (let j = i + 1; j < items.length; j++) {
-      let sum = items[i] + items[j];
+      let sum = items[i].value + items[j].value;
       if (sum === credit) {
-        return `${i + 1} ${j + 1}`;
+        return indexesAscending(items[i].originalIndex, items[j].originalIndex);
       }
     }
   }
