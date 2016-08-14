524dfd1ec2db674939c07eb0490c35f836f4f04a
Toby Flemming <tobyf93@hotmail.com>
2016-08-14 19:18:17 +1000

    Finsh 'Minimum Scalar Product'

--- 2008/1a/minimum-scalar-product/index.js
+++ 2008/1a/minimum-scalar-product/index.js
@@ -1,7 +1,38 @@
 const { forEachCase, parseIntArray } = require('./helpers.js');
 const input = process.argv[2];
 
+const ascending = (a, b) => a - b;
+const descending = (a, b) => b - a;
+
+// const sortVectors = (vectors) => {
+//   let lowVectors = [];
+//   let highVectors = [];
+//
+//   vectors.forEach((vector) => {
+//     if (vector > 0) {
+//       highVectors.push(vector);
+//     } else {
+//       lowVectors.push(vector);
+//     }
+//   });
+//
+//   lowVectors.sort(sortAscending);
+//   highVectors.sort(sortAscending);
+//
+//   return [lowVectors, highVectors];
+// };
+
 forEachCase(input, (args) => {
-  const xVectors = parseIntArray(args[1]);
-  const yVectors = parseIntArray(args[2]);
+  let xVectors = parseIntArray(args[1]);
+  let yVectors = parseIntArray(args[2]);
+
+  xVectors.sort(ascending);
+  yVectors.sort(descending);
+
+  let total = 0;
+  for (let i = 0; i < xVectors.length; i++) {
+    total += xVectors[i] * yVectors[i];
+  }
+
+  return total;
 });
