73169e4603d06341fc146a10c436e59167f9e300
Toby Flemming <toby.flemming@williamhill.com.au>
2016-08-25 11:34:15 +1000

    Remove unwanted code

--- 2008/1a/minimum-scalar-product/index.js
+++ 2008/1a/minimum-scalar-product/index.js
@@ -4,24 +4,6 @@ const input = process.argv[2];
 const ascending = (a, b) => a - b;
 const descending = (a, b) => b - a;
 
-// const sortVectors = (vectors) => {
-//   let lowVectors = [];
-//   let highVectors = [];
-//
-//   vectors.forEach((vector) => {
-//     if (vector > 0) {
-//       highVectors.push(vector);
-//     } else {
-//       lowVectors.push(vector);
-//     }
-//   });
-//
-//   lowVectors.sort(sortAscending);
-//   highVectors.sort(sortAscending);
-//
-//   return [lowVectors, highVectors];
-// };
-
 forEachCase(input, (args) => {
   let xVectors = parseIntArray(args[1]);
   let yVectors = parseIntArray(args[2]);
